3f636098a29e76a3c54477289908766f86c653ad
funk222 <59368285+funk222@users.noreply.github.com>
2020-02-11 23:58:02 -0500

    lateralTuning.pid.kiV = [[0.25], [0.04]]

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -230,8 +230,8 @@ class CarInterface(CarInterfaceBase):
       ret.steerRatio = 16.2
       tire_stiffness_factor = 0.444
       ret.mass = 4370.0 * CV.LB_TO_KG + STD_CARGO_KG
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.3], [0.05]]        //[[0.3], [0.05]]
-      ret.lateralTuning.pid.kf = 0.00001   # full torque for 20 deg at 80mph means 0.00007818594      //0.00007
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.25], [0.04]]        //[[0.3], [0.05]]
+      ret.lateralTuning.pid.kf = 0.00007   # full torque for 20 deg at 80mph means 0.00007818594      //0.00007
 
     ret.steerRateCost = 1.
     ret.centerToFront = ret.wheelbase * 0.44
