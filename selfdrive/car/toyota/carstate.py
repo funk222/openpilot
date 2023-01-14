135b788242563f7e58c9e129431bf4da82dc07a7
funk222 <59368285+funk222@users.noreply.github.com>
2023-01-13 21:11:00 -0500

    Update carstate.py
    back to normal steering direction

--- selfdrive/car/toyota/carstate.py
+++ selfdrive/car/toyota/carstate.py
@@ -57,7 +57,7 @@ class CarState(CarStateBase):
           self.needs_angle_offset = False
           self.angle_offset = ret.steeringAngleDeg - angle_wheel
     else:
-      ret.steeringAngleDeg = -((cp.vl["STEER_ANGLE_SENSOR"]['STEER_ANGLE'] + cp.vl["STEER_ANGLE_SENSOR"]['STEER_FRACTION'])+23)
+      ret.steeringAngleDeg = (cp.vl["STEER_ANGLE_SENSOR"]['STEER_ANGLE'] + cp.vl["STEER_ANGLE_SENSOR"]['STEER_FRACTION'])
 
     ret.steeringRateDeg = cp.vl["STEER_ANGLE_SENSOR"]['STEER_RATE']
 
